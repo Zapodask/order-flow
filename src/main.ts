04bb02b7f651151fab0ad564f75903b02a15fff5
zapodask <joaopedroemerim@gmail.com>
2024-01-11 23:43:05 -0300

    feat: rabbitmq config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
@@ -15,12 +16,30 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const PORT = process.env.PORT || 3000;
-  const logger = new Logger('Bootstrap');
-  logger.log(`Running on ${PORT} port.`);
+  const queues = ['payment'];
+
+  for (const queue of queues) {
+    app.connectMicroservice({
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL],
+        queue: queue,
+        noAck: false,
+        queueOptions: {
+          durable: true,
+        },
+      },
+    });
+  }
+
+  await app.startAllMicroservices();
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const PORT = process.env.PORT || 3000;
+  const logger = new Logger('Bootstrap');
+  logger.log(`Running on ${PORT} port.`);
 
   await app.listen(3000);
 }
